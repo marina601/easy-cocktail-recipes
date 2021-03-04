0462e23f1b52c795a68aa67f6ba327c756f11926
marina601 <marina601@live.com>
2021-03-04 12:31:16 +0000

    Added an event listener to be able to close the bio <div>

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,22 +14,52 @@ $(".toggle-btn").click(function() {
 });
 //toggle button to display menu items at mobile screen view 
 
+//About Page:
 
-$(".img-bartender-1").click(function() {
+$(".img-bartender-1").on("click", function() {
     
 //on click expand the div 
-    $("#bio-1").height(1100);
+    $("#bio-1").height(1070);
 // on click show the text content for Olivia
-    $(".bio").delay(1000).toggle("medium");
+    $(".bio").delay(1000).show("medium");
+    $("#closeO").delay(2000).slideDown("medium");
 });
 
-$(".img-bartender-2").click(function() {
+$("#closeO").on("click", function(){
+    
+// on click hide all the elements 
+   $(".bio").slideUp("fast");
+   $(this).slideUp("fast");
+  
+   $("#bio-1").delay(3000).height(330);
+});
+
+
+$(".img-bartender-2").on("click", function() {
     
 //on click expand the div 
-    $("#bio-2").height(1000);
-// on click show the text content for martin
-    $(".bio-2").delay(1000).toggle("medium");
+   $("#bio-2").height(1000);
+// on click show the text content for martin and button
+   $(".bio-2").delay(1000).show("medium");
+   $("#closeM").delay(2000).slideDown("medium");
+
 });
 
+$("#closeM").on("click", function(){
+    
+// on click hide all the elements 
+   $(".bio-2").slideUp("fast");
+   $(this).slideUp("fast");
+  
+   $("#bio-2").delay(3000).height(330);
+});
+
+
+
+
+
+
+
+//$(".img-bartender-2").off();
 
 });
