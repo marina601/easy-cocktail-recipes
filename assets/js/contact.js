d0d2a82f14519f88551d5aa12439a9d6e7aba473
marina601 <marina601@live.com>
2021-03-09 17:38:18 +0000

    Adding alert message on send form function

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,28 @@
-console.log("hello");
+// Sending the form 
 
-function sendEmail(contactForm) {
-    emailjs.send("service_bike43o", "template_rh5u5i9", {
-        "from_name": contactForm.full-name.value, 
-        "form_email": contactForm.email-address.value,
-        "enquire": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("Email Send", response);
-        },
-        function(error) {
-            console.log("Error", error);
-        }
-    );
-    return false;
-};
+const btn = document.getElementById('submit');
+// identifying submit button 
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+   event.preventDefault(); //preventing default submit 
+
+   btn.value = 'Sending...'; //displayin new message when sending 
+
+   const serviceID = 'default_service';
+   const templateID = 'template_rh5u5i9';
+
+   emailjs.sendForm(serviceID, templateID, this) //sending the form
+    .then(() => {
+      btn.value = 'Submit';
+      let userName = document.getElementById('from_name'); //getting the username from input field
+      
+      alert(`Thank you ${userName.value}. Your message has been sent! We weill be in touch soon!`);
+      //alert shows to the user when the message has been sent 
+    }, (err) => {
+        //error message for the user when the email is not sent 
+      btn.value = 'Submit';
+      alert(alert(JSON.stringify(err)`Sorry ${userName.value} something went wrong`));
+      console.log(error);
+    });
+});
+
