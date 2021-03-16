79548118f34c459b37ad717dc908d53c741f95e6
marina601 <marina601@live.com>
2021-03-16 14:36:16 +0000

    Added event listener to images to toggle the hidden elements

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,4 +1,4 @@
-const URL = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
+const URL = "https://thecocktaildb.com/api/json/v1/1/search.php?f=m";
 
 /**const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
@@ -12,6 +12,7 @@ searchForm.addEventListener("submit", (e) => {
   console.log(searchQuery);//check that the query is working
   getCocktail();
 });*/
+
 
 
 function getCocktail(){
@@ -45,7 +46,7 @@ function displayCocktail(data){
             <div class="card-body">
             <h3 class="card-title" id="margaritaHeading">${cocktail.strDrink}</h3>
             <h4 class="card-title hidden">Ingredients</h4>
-            <ul class="list-group list-group-flush ingredients" id="ing-margarita">
+            <ul class="list-group list-group-flush ingredients hidden">
                 ${cocktail.strIngredient1 ? `<li class="list-group-item">${cocktail.strIngredient1} ${cocktail.strMeasure1 ? ` : ${cocktail.strMeasure1}` : ""} </li>` : ""}
                 ${cocktail.strIngredient2 ? `<li class="list-group-item">${cocktail.strIngredient2} ${cocktail.strMeasure2 ? ` : ${cocktail.strMeasure2}` : ""} </li>` : ""}
                 ${cocktail.strIngredient3 ? `<li class="list-group-item">${cocktail.strIngredient3} ${cocktail.strMeasure3 ? ` : ${cocktail.strMeasure3}` : ""} </li>` : ""}
@@ -54,17 +55,33 @@ function displayCocktail(data){
                 ${cocktail.strIngredient6 ? `<li class="list-group-item">${cocktail.strIngredient6} ${cocktail.strMeasure6 ? ` : ${cocktail.strMeasure6}` : ""} </li>` : ""}
                 ${cocktail.strIngredient7 ? `<li class="list-group-item">${cocktail.strIngredient7} ${cocktail.strMeasure7 ? ` : ${cocktail.strMeasure7}` : ""} </li>` : ""}
             </ul>
-            <h4 class="card-title">Method</h4>
-            <p class="card-text" id="margarita-method">${cocktail.strInstructions}</p>
-            <p class="card-text"><strong>Category : </strong>${cocktail.strAlcoholic}</p>
+            <h4 class="card-title hidden">Method</h4>
+            <p class="card-text hidden" id="margarita-method">${cocktail.strInstructions}</p>
+            <p class="card-text hidden"><strong>Category : </strong>${cocktail.strAlcoholic}</p>
             </div>
           </div>
       </div>
     `
   });
   $('#cocktail-list').empty().append(cocktailElems)
+  
+   const cocktailImg = document.querySelector("#cocktail-list img");
+   console.log(cocktailImg);
+
+   $(cocktailImg).on("click", function(){ 
+       $(".hidden").toggle();
+    }); 
+
+
 }
-getCocktail()
+
+getCocktail();
+
+
+//Adding event listener 
+
+
+
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -48,4 +48,5 @@ $("#closeM").on("click", function(){
 });
 
 
+
 });
