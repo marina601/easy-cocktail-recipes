820be4289d41a413b519055e65a0fa07dfe3fccd
marina601 <marina601@live.com>
2021-03-18 20:33:45 +0000

    Fix: the transition of bio div closing allowing all the elements slide up first

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ $(".toggle-btn").click(function() {
 $(".img-bartender-1").on("click", function() {
     
 //on click expand the div 
-    $("#bio-1").height(1070);
+    $("#bio-1").height(1000);
 // on click show the text content for Olivia
     $(".bio").show("medium");
     $("#closeO").slideDown("medium");
@@ -26,14 +26,14 @@ $("#closeO").on("click", function(){
    $(".bio").slideUp("fast");
    $(this).slideUp("fast");
   
-   $("#bio-1").delay(3000).height(330);
+   $("#bio-1").delay(3500).height(330);
 });
 
 
 $(".img-bartender-2").on("click", function() {
     
 //on click expand the div 
-   $("#bio-2").height(1020);
+   $("#bio-2").height(1000);
 // on click show the text content for martin and button
    $(".bio-2").show("medium");
    $("#closeM").slideDown("medium");
@@ -46,7 +46,7 @@ $("#closeM").on("click", function(){
    $(".bio-2").slideUp("fast");
    $(this).slideUp("fast");
   
-   $("#bio-2").delay(3000).height(330);
+   $("#bio-2").delay(3500).height(330);
 });
 
 
