fcf010571d4ed06e648c0a61a36f7999ce9a98a3
marina601 <marina601@live.com>
2021-03-19 20:34:52 +0000

    Added an event listener on mouseleave to close the recipe card

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -29,24 +29,26 @@ function displayCocktail(data){
   data.drinks.forEach(cocktail => {
     cocktailElems += `
           <div class="col-12 col-md-6 col-lg-4">
-          <div class="card" id="margharita">
+          <div class="card">
             <img src="${cocktail.strDrinkThumb}" class="card-img-top" alt="${cocktail.strDrinkThumb}">
                 <div class="card-body">
                 <h3 class="card-title">${cocktail.strDrink}</h3>
-                <h4 class="card-title hidden">Ingredients</h4>
-                <ul class="list-group list-group-flush ingredients hidden">
-                ${cocktail.strIngredient1 ? `<li class="list-group-item">${cocktail.strIngredient1} ${cocktail.strMeasure1 ? ` : ${cocktail.strMeasure1}` : ""} </li>` : ""}
-                ${cocktail.strIngredient2 ? `<li class="list-group-item">${cocktail.strIngredient2} ${cocktail.strMeasure2 ? ` : ${cocktail.strMeasure2}` : ""} </li>` : ""}
-                ${cocktail.strIngredient3 ? `<li class="list-group-item">${cocktail.strIngredient3} ${cocktail.strMeasure3 ? ` : ${cocktail.strMeasure3}` : ""} </li>` : ""}
-                ${cocktail.strIngredient4 ? `<li class="list-group-item">${cocktail.strIngredient4} ${cocktail.strMeasure4 ? ` : ${cocktail.strMeasure4}` : ""} </li>` : ""}
-                ${cocktail.strIngredient5 ? `<li class="list-group-item">${cocktail.strIngredient5} ${cocktail.strMeasure5 ? ` : ${cocktail.strMeasure5}` : ""} </li>` : ""}
-                ${cocktail.strIngredient6 ? `<li class="list-group-item">${cocktail.strIngredient6} ${cocktail.strMeasure6 ? ` : ${cocktail.strMeasure6}` : ""} </li>` : ""}
-                ${cocktail.strIngredient7 ? `<li class="list-group-item">${cocktail.strIngredient7} ${cocktail.strMeasure7 ? ` : ${cocktail.strMeasure7}` : ""} </li>` : ""}
-                </ul>
-                <h4 class="card-title hidden">Method</h4>
-                <p class="card-text hidden" id="margarita-method">${cocktail.strInstructions}</p>
-                <p class="card-text hidden"><strong>Glass Type : </strong>${cocktail.strGlass}</p>
-                <p class="card-text hidden"><strong>Category : </strong>${cocktail.strAlcoholic}</p>
+                  <div class="recipe hidden">
+                    <h4 class="card-title">Ingredients</h4>
+                    <ul class="list-group list-group-flush ingredients">
+                    ${cocktail.strIngredient1 ? `<li class="list-group-item">${cocktail.strIngredient1} ${cocktail.strMeasure1 ? ` : ${cocktail.strMeasure1}` : ""} </li>` : ""}
+                    ${cocktail.strIngredient2 ? `<li class="list-group-item">${cocktail.strIngredient2} ${cocktail.strMeasure2 ? ` : ${cocktail.strMeasure2}` : ""} </li>` : ""}
+                    ${cocktail.strIngredient3 ? `<li class="list-group-item">${cocktail.strIngredient3} ${cocktail.strMeasure3 ? ` : ${cocktail.strMeasure3}` : ""} </li>` : ""}
+                    ${cocktail.strIngredient4 ? `<li class="list-group-item">${cocktail.strIngredient4} ${cocktail.strMeasure4 ? ` : ${cocktail.strMeasure4}` : ""} </li>` : ""}
+                    ${cocktail.strIngredient5 ? `<li class="list-group-item">${cocktail.strIngredient5} ${cocktail.strMeasure5 ? ` : ${cocktail.strMeasure5}` : ""} </li>` : ""}
+                    ${cocktail.strIngredient6 ? `<li class="list-group-item">${cocktail.strIngredient6} ${cocktail.strMeasure6 ? ` : ${cocktail.strMeasure6}` : ""} </li>` : ""}
+                    ${cocktail.strIngredient7 ? `<li class="list-group-item">${cocktail.strIngredient7} ${cocktail.strMeasure7 ? ` : ${cocktail.strMeasure7}` : ""} </li>` : ""}
+                    </ul>
+                    <h4 class="card-title">Method</h4>
+                    <p class="card-text">${cocktail.strInstructions}</p>
+                    <p class="card-text"><strong>Glass Type : </strong>${cocktail.strGlass}</p>
+                    <p class="card-text"><strong>Category : </strong>${cocktail.strAlcoholic}</p>
+                  </div>
                 </div>
           </div>
       </div>
@@ -58,11 +60,25 @@ function displayCocktail(data){
   //Adding event listener 
    let card = document.querySelectorAll(".card-body");
        for (let i = 0; i < card.length; i ++) {
+         $(card[i]).click(function(){
+           if($(this).children(".recipe").hasClass("hidden")){
+           $(this).children(".recipe").toggleClass("hidden");
+         }
+         });
+
+         $(card[i]).mouseleave(function(){
+           $(this).children(".recipe").toggleClass("hidden");
+         });
+       } 
+
+
+     /**  let card = document.querySelectorAll(".card-body");
+       for (let i = 0; i < card.length; i ++) {
             $(card[i]).on("click", function(){ 
              $(this).children(".hidden").toggle(); 
             });
        
-    }
+    }*/
        
 }
 
