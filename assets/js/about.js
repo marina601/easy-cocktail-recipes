506ecedb7ff30beeec5ed9c767321136ae7e4821
marina601 <marina601@live.com>
2021-03-31 20:35:11 +0000

    Fix: the div expanding on click with if else statement

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,51 +1,56 @@
 //jshint esversion: 6
 /*globals $:false */
-
 $(document).ready(function(){
-    //About Page:
-
-$(".img-bartender-1, #headingOlivia").on("click", function() {
-    
-//on click expand the div 
-    $("#bio-1").css("height", "fit-content");
-// on click show the text content for Olivia
-    $(".bio").show(450);
-    $("#solialLinks1").show(470);
-    $("#closeO").slideDown(500);
-});
-
-
-    
-// on click hide all the elements
- 
-
-$("#closeO").on("click", function(){
-    
-// on click hide all the elements 
-   $(".bio, #solialLinks1").slideUp(100);
-   $(this).slideUp(100);
-   $("#bio-1").delay(2000).height(330);
-});
-
-
-$(".img-bartender-2, #headingMartin").on("click", function() {
-    
-//on click expand the div 
-   $("#bio-2").css("height", "fit-content");
-// on click show the text content for martin and button
-   $(".bio-2").show(450);
-   $("#socialLinks2").show(470);
-   $("#closeM").slideDown(500);
-
-});
-
-$("#closeM").on("click", function(){
-    
-// on click hide all the elements 
-   $(".bio-2, #socialLinks2").slideUp(100);
-   $(this).slideUp(100);
   
-   $("#bio-2").delay(2000).height(330);
-});
+// Declaring variables 
+const img = document.querySelectorAll("img");
+const fullName = document.querySelectorAll("h3");
+const closeBtn = document.querySelectorAll(".close-btn");
+const bioDivs = document.querySelectorAll(".bioDiv");
+
+
+// function to display the hidden information on click 
+function showBio(){
+    for (let i = 0; i < 2; i++){
+       let eachName = fullName[i];
+       let singleImg = img[i];
+     
+      
+        $(eachName).on("click", function(){
+          $(this).nextAll().show(200);
+           $(this).parent(bioDivs).css("height", "fit-content");
+        });
+        $(singleImg).on("click", function(){
+          $(this).nextAll().show(200);
+           $(this).parent(bioDivs).css("height", "fit-content");
+        });
+    }
+
+}
+
+showBio();
+
+
+//function to hide the information on click of the button
+function closeBio(){
+    for(let j = 0; j < closeBtn.length; j++){
+        $(closeBtn[j]).on("click", function(){
+          $(this).prevUntil(fullName).hide();
+          $(this).hide();
+          $(this).delay(1000).parent(bioDivs).height(350);
+        });
+    }
+}
+
+closeBio();
+
+//if else statement to check the height of the bioDivs and to ensure only one is opening at the time
+if ($("#bio-1").height() > 1000) {
+        $("#bio-2").height(354);
+} else if ($("bio-2").height() > 100) {
+    $("#bio-2").height(354);
+} else {
+    $(bioDivs).height(350);
+}
 
 });
