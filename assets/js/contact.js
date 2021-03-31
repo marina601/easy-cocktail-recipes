692cc74fa435e4863b31cbf35a4247ab3c27e32d
marina601 <marina601@live.com>
2021-03-31 13:03:55 +0000

    added return false to the function to stop the window from loading a new page

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,40 +1,35 @@
 //jshint esversion: 6
-// Sending the form 
-// identifying submit button and contact form variables 
+
+// declaring variables
 const btn = document.getElementById('submit');
 const contactForm = document.getElementById('contact-form');
-//Setting variables for serviceID and templateID
+const userName = document.getElementById('from_name'); //getting the username from input field
+// declaring variables for serviceID and templateID
 const serviceID = 'default_service';
 const templateID = 'template_rh5u5i9';
  
 //Code was taken from emailJS to set up the initial function
+window.onload = function() {
 contactForm.addEventListener('submit', function(event) {
    event.preventDefault(); //preventing default submit 
-
    btn.value = 'Sending...'; //displaying new button value when sending 
 
    emailjs.sendForm(serviceID, templateID, this) //sending the form
     .then(() => {
-    
       btn.value = 'Submit';
-      let userName = document.getElementById('from_name'); //getting the username from input field
-      
       alert(`Thank you ${userName.value}. Your message has been sent! We will be in touch soon!`);
       //alert shows to the user when the message has been sent 
-
       contactForm.reset(); //reset the form on submit 
-    
-     
     }, (err) => {
         //error message for the user when the email is not sent 
       btn.value = 'Submit';
       alert(alert(JSON.stringify(err)`Sorry ${userName.value} something went wrong`));
-      console.log(error);
     });
 
-    
+    return false;  // To stop from loading a new page
 });
 
+};
 
 
 
