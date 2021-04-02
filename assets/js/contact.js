7fef5f951f1b0134c27ca518632e9ecd56e4b997
marina601 <marina601@live.com>
2021-04-02 20:19:21 +0000

    Added timeout function on line 49

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -25,28 +25,28 @@ const templateID = 'template_rh5u5i9';
  * on submit add event listener 
  * prevent default submit 
  * button value changes once the button is pressed 
- * disabling the button after submit to prevent the user from submitting the form again
  */
 
 window.onload = function () {
     contactForm.addEventListener('submit', function (event) {
         event.preventDefault();
         btn.value = 'Sending...';
-        btn.disabled = true;
 
         /**
          * Submitting the form 
          * Submit button value changed to submit once the form is sent 
+         * time out function to disable the submit button once the form has been submitted 
          * Alert letting the user know the message has been sent
          * Reset the form 
          * Incase of an error 
-         * Submit button value is disabled 
+         * Submit button is disabled 
          * Alert shows letting the user know the message has not been sent 
          */
 
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
                 btn.value = 'Submit';
+                setTimeout(() => { btn.disabled = true; }, 3000);
                 alert(`Thank you ${userName.value}. Your message has been sent! We will be in touch soon!`);
                 contactForm.reset();
 
